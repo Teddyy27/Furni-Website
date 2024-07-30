3d0733f129ef762b5826ab4e0c8bd99b00453b97
Arnav <arnavagarwal123654@gmail.com>
2024-07-30 17:07:16 +0530

    hamburger and 8th

--- header.js
+++ header.js
@@ -1,17 +1,45 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// const nav_menu = document.querySelector('.material-symbols-outlined');
+// const ham_menu = document.querySelector('.hamburger-menu');
 
-// Get the header
-var header = document.getElementById("myHeader");
+// nav_menu.addEventListener('click', () => {
+//     nav_menu.classList.toggle('active');
+//     ham_menu.classList.toggle('active');
+// });
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+// function openNav() {
+//     document.getElementById("hamburger-menu").style.width = "100%";
+// }
+  
+// function closeNav() {
+//     document.getElementById("hamburger-menu").style.width = "0%";
+// }
+/* Set the width of the sidebar to 250px (show it) */
+// function openNav() {
+//     document.getElementById("hamburger-menu").style.width = "250px";
+//   }
+  
+//   /* Set the width of the sidebar to 0 (hide it) */
+//   function closeNav() {
+//     document.getElementById("hamburger-menu").style.width = "0";
+//   }
+// function toggleMobileMenu(menu){
+//     menu.classList.toggle('open');
+// }
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.scrollY > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+//
+//
+// function toggleMobileMenu() {
+//     var menu = document.querySelector('.mobile-menu');
+//     if (menu.style.display === 'block') {
+//         menu.style.display = 'none';
+//     } else {
+//         menu.style.display = 'block';
+//     }
+// }
+
+function toggleMobileMenu() {
+    var icon = document.getElementById('hamburger-icon');
+    var menu = document.querySelector('.mobile-menu');
+    icon.classList.toggle('open');
+    menu.classList.toggle('open');
 }
